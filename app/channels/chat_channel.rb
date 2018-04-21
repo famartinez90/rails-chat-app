741969feccd5e151770e4aa7233fe64e91bb8dda
Federico Martinez <famfededeveloper@gmail.com>
2018-04-21 13:13:56 -0300

    Fomateo

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,13 +1,13 @@
 class ChatChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from "chat_channel"
-  end
+	def subscribed
+		stream_from "chat_channel"
+	end
 
-  def unsubscribed
-  end
+	def unsubscribed
+	end
 
-  def speak(data)
-    Message.create(:id_group => data["id_group"], :from => data['user'], :content => data["message"])
-    ActionCable.server.broadcast "chat_channel", data
-  end
+	def speak(data)
+		Message.create(:id_group => data["id_group"], :from => data['user'], :content => data["message"])
+		ActionCable.server.broadcast "chat_channel", data
+	end
 end
