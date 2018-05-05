bad51f452328acff2bc2b5872fed64b607d87bff
Federico Martinez <famfededeveloper@gmail.com>
2018-05-05 00:30:04 -0300

    Permite enviar audios grabados con el microfono

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -5,9 +5,94 @@
 //= require_self
 //= require_tree ./channels/chat/
 
-(function() {
+(function () {
 	this.App || (this.App = {});
 
 	App.cable = ActionCable.createConsumer();
+
+	document.addEventListener('DOMContentLoaded', function () {
+		var record = document.querySelector('.record');
+		var stop = document.querySelector('.stop');
+		var upload = document.querySelector('.upload');
+		var type = document.getElementById('type');
+		var id_group = document.getElementById('id_group');
+		var message_to = document.getElementById('to');
+
+		upload.onclick = function () {
+			var file = document.getElementById('picture');
+
+			if (file) {
+				var fd = new FormData();
+				fd.append('picture', file.files[0]);
+				fd.append('type', type.innerText);
+	
+				if (id_group) {
+					fd.append('id_group', id_group.innerText);
+				}
+	
+				if (message_to) {
+					fd.append('message_to', message_to.innerText);
+				}
+	
+				var XHR = new XMLHttpRequest();
+				XHR.open('POST', '/upload/picture');
+				XHR.send(fd);
+
+				file.value = '';
+			}
+		}
+
+		if (navigator.mediaDevices.getUserMedia) {
+			var constraints = { audio: true };
+			var chunks = [];
+
+			var onSuccess = function (stream) {
+				var mediaRecorder = new MediaRecorder(stream);
+
+				record.onclick = function () {
+					mediaRecorder.start();
+				}
+
+				stop.onclick = function () {
+					mediaRecorder.stop();
+				}
+
+				mediaRecorder.onstop = function (e) {
+					var blob = new Blob(chunks, { 'type': 'audio/ogg; codecs=opus' });
+					chunks = [];
+
+					var fd = new FormData();
+					fd.append('audio_name', 'record.ogg');
+					fd.append('audio', blob);
+					fd.append('type', type.innerText);
+
+					if (id_group) {
+						fd.append('id_group', id_group.innerText);
+					}
+
+					if (message_to) {
+						fd.append('message_to', message_to.innerText);
+					}
+
+					var XHR = new XMLHttpRequest();
+					XHR.open('POST', '/upload/audio');
+					XHR.send(fd);
+				}
+
+				mediaRecorder.ondataavailable = function (e) {
+					chunks.push(e.data);
+				}
+			}
+
+			var onError = function (err) {
+				console.log('The following error occured: ' + err);
+			}
+
+			navigator.mediaDevices.getUserMedia(constraints).then(onSuccess, onError);
+
+		} else {
+			console.log('getUserMedia not supported on your browser!');
+		}
+	}, false);
 
 }).call(this);
--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -1,4 +1,6 @@
 class UploadController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
     def upload_picture
         picture = params[:picture]
         newName = params[:type] + '_picture-' + Time.now.strftime('%Y%m%d%H%M%S') + '.' + picture.original_filename.split('.').drop(1).join('.')
@@ -8,6 +10,18 @@ class UploadController < ApplicationController
         end
 
         createPictureMessageFromTypeAndBroadcast(params[:type], cookies.encrypted[:user], '/uploads/' + newName, params[:id_group], params[:message_to])
+    end
+
+    def upload_audio
+        audio = params[:audio]
+
+        newName = params[:type] + '_audio-' + Time.now.strftime('%Y%m%d%H%M%S') + '.' + params[:audio_name].split('.').drop(1).join('.')
+
+        File.open(Rails.root.join('public', 'uploads', newName), 'wb') do |file|
+            file.write(audio.read)
+        end
+
+        createAudioMessageFromTypeAndBroadcast(params[:type], cookies.encrypted[:user], '/uploads/' + newName, params[:id_group], params[:message_to])
     end
     
     private 
@@ -22,7 +36,21 @@ class UploadController < ApplicationController
         elsif type == 'private'
             PrivateMessage.create({:from => from, :to => to, :content => content, :messageType => 'picture'})
             ActionCable.server.broadcast "private::#{from}::#{to}::messages", { from: from, to: to, picture: { name: content.split('/').last, href: content } }
-		        ActionCable.server.broadcast "private::#{to}::#{from}::messages", { from: from, to: to, picture: { name: content.split('/').last, href: content } }
+            ActionCable.server.broadcast "private::#{to}::#{from}::messages", { from: from, to: to, picture: { name: content.split('/').last, href: content } }
+        end
+    end
+    
+    def createAudioMessageFromTypeAndBroadcast(type, from, content, groupId, to)
+        if type == 'lobby'
+            LobbyMessage.create(:from => from, :content => content, :messageType => 'audio')
+            ActionCable.server.broadcast "lobby:messages", { from: from, audio: { name: content.split('/').last, href: content }  }
+        elsif type == 'group'
+            GroupMessage.create(:id_group => groupId, :from => from, :content => content, :messageType => 'audio')
+            ActionCable.server.broadcast "group" + groupId + ":messages", { from: from, audio: { name: content.split('/').last, href: content }  }
+        elsif type == 'private'
+            PrivateMessage.create({:from => from, :to => to, :content => content, :messageType => 'audio'})
+            ActionCable.server.broadcast "private::#{from}::#{to}::messages", { from: from, to: to, audio: { name: content.split('/').last, href: content } }
+            ActionCable.server.broadcast "private::#{to}::#{from}::messages", { from: from, to: to, audio: { name: content.split('/').last, href: content } }
         end
     end
 end
